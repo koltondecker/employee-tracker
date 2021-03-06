f4074df119c311db5bc50c1b91e17f1b54c0cd4b
Kolton Decker <deckerman25@gmail.com>
2021-03-06 14:19:19 -0600

    Added function to update role for an employee.

--- testfile.js
+++ testfile.js
@@ -290,7 +290,7 @@ const addDepartment = () => {
 
 const removeEmployee = () => {
 
-    const employeesArray = [];
+    let employeesArray = [];
 
     connection.query(
         `SELECT id, first_name, last_name FROM employee`,
@@ -329,8 +329,55 @@ const removeEmployee = () => {
 
 const updateEmployeeRole = () => {
 
-    //TODO: do this function!!!
+    let employeesArray = []
 
+    connection.query(
+        `SELECT first_name, last_name FROM employee`,
+        (err, res) => {
+
+            if (err) throw err;
+
+            inquire.prompt([
+                {
+                    type: "list",
+                    name: "employee",
+                    message: "Which employee would you like to update the role for?",
+                    choices() {
+                        res.forEach(e => {
+                            employeesArray.push(`${e.first_name} ${e.last_name}`);
+                        });
+                        return employeesArray;
+                    }
+                },
+                {
+                    type: "input",
+                    name: "role",
+                    message: "What is the new role id?"
+                }
+            ]).then( (answers) => {
+        
+                console.log(answers);
+
+                const updateEmployeeRole = answers.employee.split(' ');
+                const updateEmployeeRoleFirstName = JSON.stringify(updateEmployeeRole[0]);
+                const updateEmployeeRoleLastName = JSON.stringify(updateEmployeeRole[1]);
+
+                connection.query(
+                    `UPDATE employee
+                    SET role_id = ${answers.role}
+                    WHERE first_name = ${updateEmployeeRoleFirstName}
+                    AND last_name = ${updateEmployeeRoleLastName}`,
+
+                    (err, res) => {
+
+                        if (err) throw err;
+
+                        mainMenu();
+                    }
+                );
+            });
+        }
+    );  
 };
 
 const updateEmployeeManager = () => {
