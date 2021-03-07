b6563dc996b37c863ff274017be9736f76bdcb5a
Kolton Decker <deckerman25@gmail.com>
2021-03-07 15:57:00 -0600

    Added function to remove department from database.

--- Public/assets/js/removeDepartment.js
+++ Public/assets/js/removeDepartment.js
@@ -6,8 +6,36 @@ const index = require('../../../index');
 
 const removeDepartment = () => {
 
+    let departmentsArray = [];
 
+    connection.query(
+        `SELECT department FROM department`,
+        (err, res) => {
 
+            inquire.prompt([
+                {
+                    type: "list",
+                    name: "department",
+                    message: "Which department would you like to remove?",
+                    choices() {
+                        res.forEach(e => {
+                            departmentsArray.push(e.department);
+                        })
+                        return departmentsArray;
+                    }
+                }
+            ]).then( (answer) => {
+
+                connection.query(
+                    `DELETE FROM department
+                    WHERE department = ${JSON.stringify(answer.department)}`
+                );
+
+                index.mainMenu();
+
+            }); 
+        }
+    );
 };
 
 module.exports.removeDepartment = removeDepartment;
