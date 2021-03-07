c15a25ee50d7bc7ea0ea5896e646e5f35610cecc
Kolton Decker <deckerman25@gmail.com>
2021-03-06 22:58:51 -0600

    Added function to change employoee's manager.

--- testfile.js
+++ testfile.js
@@ -380,7 +380,55 @@ const updateEmployeeRole = () => {
 
 const updateEmployeeManager = () => {
 
-    //TODO: do this function!!!
+    let employeesArray = []
+
+    connection.query(
+        `SELECT first_name, last_name FROM employee`,
+        (err, res) => {
+
+            if (err) throw err;
+
+            inquire.prompt([
+                {
+                    type: "list",
+                    name: "employee",
+                    message: "Which employee would you like to update the manager for?",
+                    choices() {
+                        res.forEach(e => {
+                            employeesArray.push(`${e.first_name} ${e.last_name}`);
+                        });
+                        return employeesArray;
+                    }
+                },
+                {
+                    type: "input",
+                    name: "managerID",
+                    message: "What is the new manager id?"
+                }
+            ]).then( (answers) => {
+        
+                console.log(answers);
+
+                const updateEmployeeRole = answers.employee.split(' ');
+                const updateEmployeeRoleFirstName = JSON.stringify(updateEmployeeRole[0]);
+                const updateEmployeeRoleLastName = JSON.stringify(updateEmployeeRole[1]);
+
+                connection.query(
+                    `UPDATE employee
+                    SET manager_id = ${answers.managerID}
+                    WHERE first_name = ${updateEmployeeRoleFirstName}
+                    AND last_name = ${updateEmployeeRoleLastName}`,
+
+                    (err, res) => {
+
+                        if (err) throw err;
+
+                        mainMenu();
+                    }
+                );
+            });
+        }
+    ); 
 
 };
 
